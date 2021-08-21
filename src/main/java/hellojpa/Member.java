771e583b294bce0b0dd66c06aaba261c82b63bae
youngsoo23 <oh.youngsoo23@gmail.com>
2021-08-21 22:55:22 +0900

    practice jpa

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,6 +1,8 @@
 package hellojpa;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -8,6 +10,8 @@ import javax.persistence.Table;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Member {
     @Id
     private Long id;
