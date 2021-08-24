10edeb66d52d4b353a31985e6a95905598eb401d
youngsoooh <oh.youngsoo23@gmail.com>
2021-08-24 15:49:28 +0900

    상속 관계 맵핑 추상클래스 수정 구현 클래스마다 생성

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -8,9 +8,9 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn
-public class Item {
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@DiscriminatorColumn // single table 일때는 생략가능
+public abstract class Item {
     @Id
     @GeneratedValue
     @Column(name = "item_id")
